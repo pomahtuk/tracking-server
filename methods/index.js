07c77fffc278f51468419ba9f5df040806b564df
Ilya Lovriakov <ilya.lovriakov@booking.com>
2014-11-17 17:17:02 +0100

    wip

--- methods/index.js
+++ methods/index.js
@@ -2,7 +2,8 @@
 
 'use strict';
 
-var ClientAccount = require('../models/clientAccount').ClientAccount; // Mongoose ODM
+var ClientAccount = require('../models/clientAccount').ClientAccount,
+  Visitor = require('../models/visitor').Visitor; // Mongoose ODM
 
 /**
  * Add your other methods below.
@@ -13,32 +14,37 @@ exports.init = function (server) {
 
   server.method("ensureCorrectDomain", function (request, next) {
     var apiKey = request.query.apiKey;
+
+    function clientTestCallback(err, client) {
+      if (err) {
+        next(err, null);
+      } else {
+        next(null, client);
+      }
+    }
+
+    function clientCallback(err, client) {
+      if (err) {
+        next(err, null);
+      // this check should be far more complicated
+      } else if (client && client.domain === request.headers.host) {
+        next(null, client);
+      } else {
+        next(new Error('No client found for this API key'), null);
+      }
+    }
+
     if (apiKey && request.headers.host) {
       if (apiKey === 'laborant_development_key' || request.headers.host.indexOf('localhost') !== -1) {
         ClientAccount
           .findOne({'name': 'pman'})
           .populate('experiments')
-          .exec(function (err, client) {
-            if (err) {
-              next(err, null);
-            } else {
-              next(null, client);
-            }
-          });
+          .exec(clientTestCallback);
       } else {
         ClientAccount
           .findOne(apiKey)
           .populate('experiments')
-          .exec(function (err, client) {
-            if (err) {
-              next(err, null);
-            // this check should be far more complicated
-            } else if (client && client.domain === request.headers.host) {
-              next(null, client);
-            } else {
-              next(new Error('No client found for this API key'), null);
-            }
-          });
+          .exec(clientCallback);
       }
     } else {
       next(new Error('no query params or wrong headers'), null);
@@ -47,9 +53,33 @@ exports.init = function (server) {
 
   server.method("getUserFromCookies", function (request, next) {
     console.log(request.state);
-    // if present - request a visitor with sessionId from cookie
-    // if not - create new visitor
-    next(null, 'placeholder');
+    var sessionId = request.state.laborantSession,
+      newVisitor;
+
+    function visitorCallback(err, visitor) {
+      if (err) {
+        next(err, null);
+      } else {
+        next(null, visitor);
+      }
+    }
+
+    if (sessionId) {
+      // if present - request a visitor with sessionId from cookie
+      Visitor.findOne(sessionId).exec(visitorCallback);
+    } else {
+      // if not - create new visitor
+      // TODO: gather all visitor info!
+      newVisitor = new Visitor({
+        sessionKey: 'key',
+        browser: 'browser',
+        lang: 'lang',
+        ip: 'ip',
+        referrer: 'referrer',
+        country: 'country'
+      });
+      newVisitor.save(visitorCallback);
+    }
   });
 
 };

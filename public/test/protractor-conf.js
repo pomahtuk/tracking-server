8ed49b1605983bd12762a78206ab43d8f85e2e59
pomahtuk <pman89@ya.ru>
2014-11-23 16:10:38 +0100

    sample client testing

---
+++ public/test/protractor-conf.js
@@ -0,0 +1,25 @@
+/*jslint node: true, es5: true, indent: 2, nomen: true*/
+
+'use strict';
+
+exports.config = {
+  allScriptsTimeout: 11000,
+
+  specs: [
+    'e2e/*.js'
+  ],
+
+  capabilities: {
+    'browserName': 'chrome'
+  },
+
+  chromeOnly: true,
+
+  baseUrl: 'http://localhost:3000/',
+
+  framework: 'jasmine',
+
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 30000
+  }
+};
---
+++ public/test/unit/demo.js
@@ -0,0 +1,10 @@
+/*jslint node: true, es5: true, indent: 2, nomen: true*/
+/*global describe, beforeEach, it, inject, expect*/
+
+'use strict';
+
+describe('demo', function () {
+  it('check if true equals to true', function () {
+    expect(true).toBe(true);
+  });
+});
